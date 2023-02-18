e91263ec6987777b3e0582714c4b02e89e9b1834
KatieMacalintal <125110058+KatieMacalintal@users.noreply.github.com>
2023-02-17 20:41:20 -0500

    cleaned up perceptron python implementation

--- posts/perceptron/perceptron.py
+++ posts/perceptron/perceptron.py
@@ -11,15 +11,14 @@ np.random.seed(0)
 class Perceptron: 
     
     """
-    If p is a Perceptron object, then after p.fit(X, y) is called, 
-    p should have an instance variable of weights called w. 
-    (w is the vector in classifier in instructions)
-    
-    p should have an instance variable called p.history, 
-    which is a list of the evolution of the score over the training period. 
-    (see Perceptron.score(X, y) below) 
+    Determines the variable of weights (w) that linearly separates X, a dataset 
+    of observations and their features, and y, the labels for each piece of data in X. 
+    Stores w as an instance variable with the Perceptron object and creates the 
+    instance variable history, which is a list of the evolution of the score (accuracy) 
+    over the training period.
+    If data is not linearly separable, a warning is raised. 
     """
-    def fit(self, X, y, max_steps = 10000): #saw this defined in the project overview
+    def fit(self, X, y, max_steps = 10000):
         # Determine n (number of data points) from X 
         n = X.shape[0]
         
@@ -29,8 +28,7 @@ class Perceptron:
         # Determine p (number of features) from X_
         p = X_.shape[1] # INCLUDING THE EXTRA COLUMN ? 
         
-        # Initialize random weight vector 
-        # self.w = w , -b 
+        # Initialize random weight vector where self.w = (w, -b) 
         self.w = np.random.rand(p)
         
         # Define y_ which stores y as a vector of -1s and 1s instead of 0s and 1s
@@ -40,39 +38,32 @@ class Perceptron:
 
         self.history = []
         
+        # Keep editing w until it reaches perfect classifcation or max_steps
         while ((self.score(X, y) != 1.0) and (steps < max_steps)):
             self.history.append(self.score(X, y))
             i = random.randint(0, n - 1)
             self.w = self.w + (((y_[i] * self.w@X_[i]) < 0) * y_[i] * X_[i])
-            #print("steps:", steps)
             steps += 1
-            #print("weights:", self.w)
-            #print()
-            
         self.history.append(self.score(X, y))
         
+        # X is not linearly separable 
         if(self.score(X, y) != 1.0): 
             warnings.warn("WARNING: Could not converge")
     
-    """
-    Returns a vector of predicted labels (0s and 1s) 
-    These are the model’s predictions for the labels on the data.
-    """
-    def predict(self, X): 
-        X_ = np.append(X, np.ones((X.shape[0], 1)), 1) #SHOULD WE BE MODIFYING X_
-        return (X_@self.w >= 0).astype(int)
-        #print("prediction:" , prediction)
-        #return prediction
     
     """
-    Returns the accuracy of the perceptron as a number between 0 and 1, 
-    with 1 corresponding to perfect classification.
+    Returns a vector of predicted labels (either 0s or 1s), which are the model's 
+    predictions for the labels on the data X. 
+    """
+    def predict(self, X):
+        X_ = np.append(X, np.ones((X.shape[0], 1)), 1)
+        return (X_@self.w > 0).astype(int)
+    
+    
+    """
+    Returns the accuracy of the perceptron as a number between 0 and 1, given 
+    the data X and the expected labels y. 1 corresponds to perfect classification.
     """
     def score(self, X, y):
-        #print("y:", y)
-        # Creates array that indicates whether prediction was correct 
-        #print("mean:", np.mean(y == self.predict(X)))
-        #correct = np.sum(y == self.predict(X))
-        #score = correct / y.size
-        #print("score:", score)
+        # Calculate the average of how often model's prediction is the same as expected label 
         return np.mean(y == self.predict(X))
