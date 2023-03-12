b60c1027aa785510b4eef6d88b5c868e07a17541
KatieMacalintal <125110058+KatieMacalintal@users.noreply.github.com>
2023-03-12 10:33:23 -0400

    rough draft of linear-regression blog post

--- posts/linear-regression/linear_regression.py
+++ posts/linear-regression/linear_regression.py
@@ -7,10 +7,13 @@ Add an extra columns of 1s to X
 def pad(X):
     return np.append(X, np.ones((X.shape[0], 1)), 1)
 
+
+
 class LinearRegression():
     
     """
-    Find and set the optimal weight vector w (which includes the bias term), 
+    Find the optimal weight vector and create it as an 
+    instance variable w (which includes the bias term), 
     using the analytical formula. 
     
     Resource: https://middlebury-csci-0451.github.io/CSCI-0451/lecture-notes/regression.html
@@ -18,76 +21,69 @@ class LinearRegression():
     def fit_analytic(self, X, y):
         X_ = pad(X)
         self.w = np.linalg.inv(X_.T@X_)@X_.T@y
-        # added (array)[:,np.newaxis]
     
+    """
+    Create instance variable w (which includes the bias term) 
+    using the formula for the gradient of the loss function 
+    and gradient descent
+    """
     def fit_gradient(self, X, y, max_iter = 1000, alpha = 0.01):
         X_ = pad(X)
         
         # Initialize random weight vector where self.w = (w, -b) 
         self.w = np.random.rand(X_.shape[1])
-        # had self.w = np.random.rand(X_.shape[1], 1)
         
-        # Initialize new instance variables 
+        # Initialize new instance variable score_history
         self.score_history = []
         
         # Precompute P and q for finding the gradient 
         P = X_.T@X_
         q = X_.T@y
-        #print(f"{np.shape(P)}\n{P=}\n")
-        #print(f"{np.shape(q)}\n{q=}\n")
         
-        prev_loss = np.inf
+        prev_loss = np.inf # Set loss to positive infinity
         
         for i in np.arange(max_iter):
             
             # Gradient step
             gradient = (P@self.w) - q
-            # print(f"{np.shape(gradient)}\n{gradient=}\n")
-            # print(f"{np.shape(self.w)}\n{self.w=}\n")
             self.w -= alpha * gradient 
             new_loss = self.loss(X_, y)
             
             # Add value to history 
             self.score_history.append(self.score(X_, y))
             
-            # Check for convergence 
-            # Note: Could also check for convergence using the change in w 
-            
+            # Check for convergence             
             if np.allclose(new_loss, prev_loss):          
                 break
             else:
                 prev_loss = new_loss
             
-            # if np.allclose(gradient, np.zeros(len(gradient))):
-            #     #print(f"{self.w=}\n")
-            #     break 
-        
-        # if not np.allclose(gradient, np.zeros(len(gradient))):
         if not np.allclose(new_loss, prev_loss):
             warnings.warn("WARNING: Could not converge")
     
     
+    """
+    Returns a vector of predicted values, which are 
+    the model's predictions for the values on the data X,
+    which is padded.
+    """
     def predict(self, X):
-        # print(f"{np.shape(X)=}")
-        # print(f"{np.shape(self.w)=}")
         return X@self.w
         
         
+    """
+    Return overall loss of the current weights w on data X, 
+    which is padded and values y, using squared error. 
+    """
     def loss(self, X, y): 
-        # CHECK THIS FUNCTION
         return (self.predict(X) - y) ** 2
 
     """
-    The coefficient of determination is always no larger than 1, 
-    with a higher value indicating better predictive performance. 
-    It can be arbitrarily negative for very bad models.
-    Note that the numerator in the fraction is just, so 
-    making the loss small makes the coefficient of determination large.
+    Determine the score of the model coefficient of determination, 
+    assuming that X is padded.
     """
     def score(self, X, y):
-        #ASSUMES X IS PADDED 
-        y_hat = self.predict(X)
-        top_sum = np.sum((y_hat - y) ** 2)
+        top_sum = np.sum(self.loss(X, y))
         y_mean = np.mean(y)
         bottom_sum = np.sum((y_mean - y) ** 2)
         return (1 - (top_sum / bottom_sum))
