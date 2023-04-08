7555c88fe9d18e445576b2e0cda1f495c83f1b72
KatieMacalintal <125110058+KatieMacalintal@users.noreply.github.com>
2023-04-07 22:01:27 -0400

    unsupervised learning: outline for both main parts

--- posts/unsupervised-learning/.ipynb_checkpoints/unsupervised_learning-checkpoint.py
+++ posts/unsupervised-learning/.ipynb_checkpoints/unsupervised_learning-checkpoint.py
@@ -1,75 +1,68 @@
 from matplotlib import pyplot as plt
 import numpy as np
+import warnings 
 
 """
-Write a function called svd_reconstruct that 
-reconstructs an image from its singular value decomposition. 
-
-Your function should have two arguments: the image to reconstruct, and the number k of singular values to use.
+Given a greyscale image (img) and the number of singular values (k) to use, 
+reconstruct the image from its singular value decomposition. 
 """
 def svd_reconstruct(img, k):
+# MAKE THE MEMORY THING AN OPTIONAL ARGUMENT HERE
+
+
+    # Get height and width from image 
+    # No third value in shape since img is in greyscale
     height, width = img.shape
     
-    data = np.array(img) # SOME OF THE VALUES ARE NEGATIVE
+    # Get the img as a 2D matrix 
+    data = np.array(img) 
     
-    # Specified a too large k 
+    # Return unmodified data if k is too large
     if ((k > height) or (k > width)):
-        # SHOULD PROBABLY DO SOMETHING ELSE HERE AS WELL 
+        warnings.warn("WARNING: k > height or k > width of given image")
         return data
     
+    # Compute a singular value decomposition (SVD)
     U, sigma, V = np.linalg.svd(data)
+
     # create the D matrix in the SVD
-    D = np.zeros_like(data, dtype=float) # matrix of zeros of same shape as A
+    D = np.zeros_like(data, dtype=float) # matrix of zeros of same shape as data
     D[:min(data.shape),:min(data.shape)] = np.diag(sigma)        # singular values on the main diagonal
     
-    
-    U_ = U[:,:k]
-    D_ = D[:k, :k]
-    V_ = V[:k, :]
+    # Approximate by using k smaller representation
+    U_ = U[:,:k] # first k columns of U
+    D_ = D[:k, :k] # top k singular values in D
+    V_ = V[:k, :] # first k rows of V
+
+    # Reconstruct and compute approximation of data
     data_ = U_ @ D_ @ V_
     return data_
-    
-def experiment(img):
-    #img is greyscale
-    fig, axarr = plt.subplots(3, 3, figsize = (7, 3))
-    
-    
-    axarr[0][0].imshow(svd_reconstruct(img, 5), cmap = "Greys")
-    axarr[0][0].axis("off")
-    axarr[0][0].set(title = "fjdskl")
-    
-    axarr[0][1].imshow(svd_reconstruct(img, 10), cmap = "Greys")
-    axarr[0][1].axis("off")
-    axarr[0][1].set(title = "fjdskl")
-    
-    axarr[0][2].imshow(svd_reconstruct(img, 15), cmap = "Greys")
-    axarr[0][2].axis("off")
-    axarr[0][2].set(title = "fjdskl")
-    
-    axarr[1][0].imshow(svd_reconstruct(img, 20), cmap = "Greys")
-    axarr[1][0].axis("off")
-    axarr[1][0].set(title = "fjdskl")
-    
-    axarr[1][1].imshow(svd_reconstruct(img, 25), cmap = "Greys")
-    axarr[1][1].axis("off")
-    axarr[1][1].set(title = "fjdskl")
-    
-    axarr[1][2].imshow(svd_reconstruct(img, 30), cmap = "Greys")
-    axarr[1][2].axis("off")
-    axarr[1][2].set(title = "fjdskl")
-    
-    axarr[2][0].imshow(svd_reconstruct(img, 35), cmap = "Greys")
-    axarr[2][0].axis("off")
-    axarr[2][0].set(title = "fjdskl")
-    
-    axarr[2][1].imshow(svd_reconstruct(img, 40), cmap = "Greys")
-    axarr[2][1].axis("off")
-    axarr[2][1].set(title = "fjdskl")
-    
-    axarr[2][2].imshow(svd_reconstruct(img, 45), cmap = "Greys")
-    axarr[2][2].axis("off")
-    axarr[2][2].set(title = "fjdskl")
-    
 
-# DO THE FIRST OPTIONAL EXTRA 
+"""
+Perform an experiment that reconstructs greyscale image (img) with 
+several different values of k.
+"""  
+def svd_experiment(img):
+    rows = 3
+    columns = 3
+    fig, axarr = plt.subplots(rows, columns) 
     
+    for i in range(rows):
+        for j in range(columns):
+            k = (i * 3 + j + 1) * 10
+            img_title = str(k) + " components"
+            axarr[i][j].imshow(svd_reconstruct(img, k), cmap="Greys")
+            axarr[i][j].axis("off")
+            axarr[i][j].set(title=img_title)
+    
+def compare_images(A, A_):
+
+    fig, axarr = plt.subplots(1, 2, figsize = (7, 3))
+
+    axarr[0].imshow(A, cmap = "Greys")
+    axarr[0].axis("off")
+    axarr[0].set(title = "original image")
+
+    axarr[1].imshow(A_, cmap = "Greys")
+    axarr[1].axis("off")
+    axarr[1].set(title = "reconstructed image")
